9650d7cab97529857247e3612476db88f13d062c
Danny Wilson <danny@onlinetouch.nl>
2013-02-20 09:46:14 +0100

    Bump Kafka version. Include Immutant archive.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject touch/immutant-kafka "0.7.0"
+(defproject touch/immutant-kafka "0.7.2"
   :repositories {"conjars" "http://conjars.org/repo/"}
   :dependencies [
     [org.scala-lang/scala-library "2.9.2"]
-    [com.twitter/kafka_2.9.2 "0.7.0"]
+    [kafka/core-kafka_2.9.2 "0.7.2"]
 ])
